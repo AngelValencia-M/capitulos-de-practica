2a51b3ece1ad979c17ecd31e4e7ee250fa71b08d
angelvalencia <angelvalencia_M@outlook.esgit config --global user.name AngelValencia-M>
2021-03-09 16:04:27 -0700

    se han modificado y se agrrego nuevo nombre

---
+++ inicios.js
@@ -0,0 +1,115 @@
+let DineroCofla = prompt("cuanto dinero tienes cofla ");
+let DineroRoberto = prompt("cuanto dinero tienes roberto ");
+let DineroPedro = prompt("cuanto dinero tienes pedro ");
+let DineroAngel = prompt("Cuanto dinero tiene Angel");
+//funcion de cambio de numero
+
+
+if(DineroCofla >=0.6 && DineroCofla <1){
+    alert("cofla, comprar helado de agua");
+}
+
+else if(DineroCofla >= 1 && DineroCofla < 1.6){
+    alert("cofla, comprar helado de crema");
+
+}
+
+else if(DineroCofla >= 1.6 && DineroCofla < 1.7){
+    alert("cofla, comprar helado de churro");
+
+
+}
+
+else if(DineroCofla >= 1.7 && DineroCofla < 2.9){
+    alert("cofla, comprar helado de cacahuate");
+}
+
+else if(DineroCofla >= 2.9){
+    alert("cofla, helado de caca o el bote d    e mierda");
+}
+else {
+    alert("cofla, No te alcanza para ningu helado");
+}
+
+//cantidad de roberto
+
+if (DineroRoberto >= 0.6 && DineroCofla < 1){
+    alert("roberto, comprar helado de agua");
+}
+
+else if (DineroRoberto >= 1 && DineroCofla < 1.6){
+    alert("roberto, comprar helado de crema");
+}
+
+else if (DineroRoberto >= 1.6 && DineroCofla < 1.7){
+    alert("roberto, comprar helado de churro");
+}
+
+else if (DineroRoberto >= 1.7 && DineroCofla < 2.9){
+    alert("roberto, comprar helado de cacahuate");
+}
+
+else if (DineroRoberto >= 2.9){
+    alert("roberto, helado de caca o el bote de mierda");
+}
+else {
+    alert("roberto, No te alcanza para ningu helado");
+}
+
+
+//dinero de pedro
+
+
+if(DineroPedro >= 0.6 && DineroCofla <1){
+    alert("pedro, comprar helado de agua");
+}
+
+else if(DineroPedro >= 1 && DineroCofla < 1.6){
+    alert("pedro, comprar helado de crema");
+}
+
+else if(DineroPedro >= 1.6 && DineroCofla < 1.7){
+    alert("pedro, comprar helado de churro");
+
+}
+
+else if(DineroPedro >= 1.7 && DineroCofla < 2.9){
+    alert("pedro, comprar helado de cacahuate");
+}
+
+else if(DineroPedro >= 2.9){
+    alert("pedro, helado de caca o el bote de mierda");
+}
+else {
+    alert("pedro, No te alcanza para ningu helado");
+}
+
+//nuevo usuario //hello
+//Dinero angel 
+
+if (DineroAngel >= 0.6 && DineroCofla <1){
+    alert("pedro, comprar helado de agua");
+}
+
+else if(DineroAngel >= 1 && DineroCofla < 1.6){
+    alert("pedro, comprar helado de crema");
+}
+
+else if(DineroAngel >= 1.6 && DineroCofla < 1.7){
+    alert("pedro, comprar helado de churro");
+
+}
+
+else if(DineroAngel >= 1.7 && DineroCofla < 2.9){
+    alert("pedro, comprar helado de cacahuate");
+}
+
+else if(DineroAngel >= 2.9){
+    alert("pedro, helado de caca o el bote de mierda");
+}
+else {
+    alert("pedro, No te alcanza para ningu helado");
+}
+
+
+
